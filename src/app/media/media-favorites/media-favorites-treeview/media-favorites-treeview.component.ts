f33f5a161210522aa473911833ef49b1ca905887
Augustine Nwafor <anwafor@eminenttechnology.com>
2019-04-03 20:20:56 +0100

    Favorite Tree View

--- src/app/media/media-favorites/media-favorites-treeview/media-favorites-treeview.component.ts
+++ src/app/media/media-favorites/media-favorites-treeview/media-favorites-treeview.component.ts
@@ -7,9 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaFavoritesTreeviewComponent implements OnInit {
 
+  public mediaFavoriteDate: Object[];
+
   constructor() { }
 
   ngOnInit() {
+    this.mediaFavoriteDate =
+      [
+        {
+          id: 1,
+          name: "Parent Folder Title",
+          date: "Jan 30, 2018 10:15:12"
+        },
+        {
+          id: 2,
+          name: "Parent Folder Title",
+          date: "Jan 30, 2018 10:15:12",
+          parentID: 1
+        },
+        {
+          id: 3,
+          name: "Parent Folder Title",
+          date: "Jan 30, 2018 10:15:12",
+          parentID: 2
+        },
+        {
+          id: 4,
+          name: "Parent Folder Title",
+          date: "Jan 30, 2018 10:15:12",
+          parentID: 3
+        },
+        {
+          id: 5,
+          name: "Parent Folder Title",
+          date: "Jan 30, 2018 10:15:12",
+          parentID: 3
+        },
+        {
+          id: 6,
+          name: "Parent Folder Title",
+          date: "Jan 30, 2018 10:15:12",
+          parentID: 3
+        },
+        {
+          id: 7,
+          name: "Parent Folder Title",
+          date: "Jan 30, 2018 10:15:12",
+          parentID: 3
+        },
+        {
+          id: 8,
+          name: "Parent Folder Title",
+          date: "Jan 30, 2018 10:15:12"
+        },
+        {
+          id: 9,
+          name: "Parent Folder Title",
+          date: "Jan 30, 2018 10:15:12"
+        },
+        {
+          id: 10,
+          name: "Parent Folder Title",
+          date: "Jan 30, 2018 10:15:12"
+        }
+      ];
   }
 
 }
