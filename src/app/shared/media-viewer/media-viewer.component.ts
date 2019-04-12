e912bdf431ddace3525d2c08e6d026056b23e1c5
Mena Esezobor <mesezobor@eminenttechnology.com>
2019-04-12 18:18:30 +0100

    Add the media viewer for image

--- src/app/shared/media-viewer/media-viewer.component.ts
+++ src/app/shared/media-viewer/media-viewer.component.ts
@@ -79,31 +79,37 @@ export class MediaViewerComponent extends BaseComponent implements OnInit, OnDes
         this.url = `http://docs.google.com/gview?url=${this.dataSource.url}&embedded=true`;
         this.trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
         break;
-      }
+      }    
+      case 'PNG':
       case 'JPG': {
-        const imageElement = window.document.querySelector('img.image');
-        console.log('element', imageElement);
-        const viewer = new Viewer(imageElement, {
-          url: 'data-original',
-          toolbar: {
-            oneToOne: true,
-            prev() {
-              viewer.prev(true);
+        setTimeout(() => {
+          console.log(this.dataSource.url)
+          this.url = this.dataSource.url;
+          const imageElement = window.document.querySelector('img.image');
+          console.log('element', imageElement);
+          const viewer = new Viewer(imageElement, {
+            url: 'data-original',
+            toolbar: {
+              oneToOne: true,
+              prev() {
+                viewer.prev(true);
+              },
+              play: true,
+              next() {
+                viewer.next(true);
+              },
+              // download() {
+              //   const a = document.createElement('a');
+              //   a.href = viewer.image.src;
+              //   a.download = viewer.image.alt;
+              //   document.body.appendChild(a);
+              //   a.click();
+              //   document.body.removeChild(a);
+              // },
             },
-            play: true,
-            next() {
-              viewer.next(true);
-            },
-            // download() {
-            //   const a = document.createElement('a');
-            //   a.href = viewer.image.src;
-            //   a.download = viewer.image.alt;
-            //   document.body.appendChild(a);
-            //   a.click();
-            //   document.body.removeChild(a);
-            // },
-          },
-        });
+          });
+        }, 20);
+    
         break;
       }
       default:
