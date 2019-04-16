3c1959dea5b41c53e7b439a52deebe0c5dd3e99c
Mena Esezobor <mesezobor@eminenttechnology.com>
2019-04-16 18:09:43 +0100

    update form control for the metadata fields

--- src/app/admin/admin-metadata-fields/admin-metadata-fields.component.ts
+++ src/app/admin/admin-metadata-fields/admin-metadata-fields.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { ListComponent } from 'src/app/shared/list/list.component';
 import { Store, Select } from '@ngxs/store';
 import { GridColumn } from 'src/app/core/models/grid.column';
@@ -16,23 +16,31 @@ import { takeWhile } from 'rxjs/operators';
   templateUrl: './admin-metadata-fields.component.html',
   styleUrls: ['./admin-metadata-fields.component.css']
 })
-export class AdminMetadataFieldsComponent extends ListComponent implements OnInit {
+export class AdminMetadataFieldsComponent extends ListComponent implements OnInit, OnDestroy {
   columns: GridColumn[] = [
     { headerText: 'Name', field: 'fieldName', width: '70' },
     { headerText: 'Type', field: 'fieldType', width: '80' },
-    { headerText: 'List', width: '150', field: 'List' },
-    { headerText: ' ', type: 'checkbox', width: '50', field: 'isUnique' }];
+    { headerText: 'List', width: '150', field: 'List' }];
+
   public data: { [key: string]: Object }[] = [{ id: 1, name: 'All Platforms' },
   { id: 2, name: 'All Register Types' }, { id: 3, name: 'All Systems' }];
   public listFields: Object = { text: 'name', value: 'id' };
-  public placeholder: string = 'Select a List';
+
+  public fieldTypeData: { [key: string]: Object }[] = [{ id: 1, name: 'Text' },
+  { id: 2, name: 'Dropdown' }, { id: 3, name: 'Combobox' }];
+  public typeListFields: Object = { text: 'name', value: 'id' };
+
+
   editLink = "<a class='remove-cls ' style='color: #0097A9 !important; text-decoration: underline !important;'>Remove</a>";
   removeLink = "<span class='e-icons e-pencil' style='color: #0097A9 !important'></span>";
+
   fieldName: string = '';
-  fieldType: string  = '';
+  fieldType: string = '';
   fieldId: number;
   metadataFieldForm: FormGroup;
   metadataField = new MetadataFields();
+
+  componentActive: boolean;
 
   @ViewChild('listview') public dialogList: any;
   @ViewChild('fieldDialog') public fieldDialogList: DialogComponent;
@@ -49,68 +57,36 @@ export class AdminMetadataFieldsComponent extends ListComponent implements OnIni
         const metadataField: MetadataFields = { ...this.metadataField, ...this.metadataFieldForm.value };
         console.log('testing create user - ', metadataField);
         this.store.dispatch(new CreateMetaDataField(metadataField));
+        this.fieldDialogList.hide();
       }
     }
   }
 
-    //     if (this.fieldId === 0) { // Create User
-    //       console.log('testing create user - ', metadataField);
-    //       // this.store.dispatch(new CreateUser(user));
-    //       // this.currentUserId$.subscribe(userId => {
-    //       //   if (userId) {
-    //       //     this.userForm.reset();
-    //       //     this.router.navigate([`/admin/users/${userId}/edit`]);
-    //       //   }
-    //       //}),
-    //       takeWhile(() => this.componentActive);
-    //     } else { // Update User
-    //       // console.log('testing update user - ', user);
-    //       // this.store.dispatch(new UpdateUser(user.userId, user));
-    //       // this.userForm.reset(this.userForm.value);
-    //     }
-    //   }
-    // } else {
-    //   // this.errorMessage = "Please correct the validation errors.";
-    // }
-    // // var permissionData = this.fieldDialogList.getSelectedItems().data;
-    // // let permissionidArray: any[] = [];
-
-    // // permissionData.forEach(permission => {
-    // //   permissionidArray.push(permission.id);
-    // // });
-
-    // // this.selectedGroups.forEach(user => {
-    // //   this.store.dispatch(new AssignToPermission(user.id, permissionidArray));
-    // // });
-
-
-    // // this.permissionDialog.hide();
-    // // this.store.dispatch(new GetPermissions());
-  // }
-  componentActive: boolean =false;
 
 
   constructor(protected store: Store,
-    private formBuilder: FormBuilder,) {
+    private formBuilder: FormBuilder, ) {
     super(store);
     this.ShowLefNav(true);
     this.componentActive = true;
   }
-  addList(action){
+  addList(action) {
     console.log('action', action.itemData);
   }
   ngOnInit() {
     this.metadataFieldForm = this.formBuilder.group({
       id: [''],
-      fieldName: [ '', [ Validators.required ] ],
-      fieldType: [ '', [ Validators.required ] ],
-      // List: [ '', [ Validators.required ] ],
-      // emailAddress: [ '', [ Validators.required, Validators.email ] ]
+      fieldName: ['', [Validators.required]],
+      fieldType: ['', [Validators.required]],
+      List: ['', [Validators.required]],
     });
     this.store.dispatch(new GetMetaDataFields());
     this.metadataFields$.subscribe(fields => {
       this.metadataFields = fields;
     });
+  }
+  ngOnDestroy(){
+    this.componentActive = false;
   }
   navigate(action) {
     console.log('action', action);
